d125f35a20cc34500bd0851ebf6d50d0ea0d3d7a
Sravanikonapalli <konapallisravani@gmail.com>
2025-03-24 23:18:49 +0530

    add live backend url

--- backend/server.js
+++ backend/server.js
@@ -73,16 +73,4 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// Middleware for authentication
-const authenticateToken = (req, res, next) => {
-    const token = req.header("Authorization")?.split(" ")[1];
-
-    if (!token) return res.status(401).json({ message: "User not logged in" });
-
-    jwt.verify(token, MY_SECRET_TOKEN, (err, user) => {
-        if (err) return res.status(403).json({ message: "Invalid token" });
-        req.user = user;
-        next();
-    });
-};
 
